09c1352af2bd1f8f2fac2d5de86f67a72aadabed
Muhammad Abdullah <113567773+AbdullahAssi@users.noreply.github.com>
2024-06-30 23:41:52 +0500

    fix

--- app/build/generated/source/buildConfig/debug/com/android/foodorderapp/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/android/foodorderapp/BuildConfig.java
@@ -5,7 +5,7 @@ package com.android.foodorderapp;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.android.foodorderapp";
+  public static final String APPLICATION_ID = "com.android.WhiskerBites";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
